562f9980d0ee02c4f998b84f178d8ce1d43e6254
Syed Hisham Akmal <hisham0502@gmail.com>
2023-04-16 12:32:01 +0530

    models.py, sqlalchemy and requirements.txt setup

--- backend/core/__init__.py
+++ backend/core/__init__.py
@@ -1,8 +1,20 @@
+import os
 from flask import Flask
 from flask_cors import CORS
+from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate 
+
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from core import views
 
