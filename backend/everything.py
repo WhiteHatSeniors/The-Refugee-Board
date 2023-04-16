cf96d73967c9d5bd1ced35f9f0639283918a4553
Sayed-Afnan-Khazi <83779299+Sayed-Afnan-Khazi@users.noreply.github.com>
2023-04-16 15:49:22 +0530

    Create working

--- backend/everything.py
+++ backend/everything.py
@@ -54,12 +54,37 @@ class Refugee(db.Model):
 with app.app_context():  
     db.create_all()
 
+@app.route('/api/post/camp',methods=["POST"])
+def createNewCamp():
+    # Recieving details of the camp
+    camp_details = request.get_json()
+    # Adding it to the database
+    new_camp = Camp(AdminName=camp_details["AdminName"],
+                    CampName=camp_details["CampName"],
+                    CampAddress=camp_details["CampAddress"],
+                    NumberOfRefugees=camp_details["NumberOfRefugees"])
+    
+    db.session.add(new_camp)
+    db.session.commit()
+    return jsonify(camp_details)
+
+
 @app.route('/api/post/refugee',methods=["POST"])
 def createNewRefugee():
-    # Recieve a refugee object
-    request_data = request.get_json()
-    # Add it to the refugeeboard database
-    # return jsonify(request_data)
+    # Recieving details of the refugee
+    refugee_details = request.get_json()
+    # Creating a new refugee object
+    new_refugee = Refugee(RefugeeID = refugee_details["RefugeeID"],
+                       CampID= refugee_details["CampID"],
+                       Name = refugee_details["Name"],
+                       Gender = refugee_details["Gender"],
+                       Age = refugee_details["Age"],
+                       CountryOfOrigin = refugee_details["CountryOfOrigin"],
+                       Message = refugee_details["Message"])
+    db.session.add(new_refugee)
+    db.session.commit()
+    return jsonify(refugee_details)
+
     
 
 # Running the app
