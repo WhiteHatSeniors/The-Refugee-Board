9484074091521ebdb9a0ac8717052012a8c82214
Sayed-Afnan-Khazi <83779299+Sayed-Afnan-Khazi@users.noreply.github.com>
2023-04-16 15:33:35 +0530

    First working version, no seperation of concerns

--- backend/core/__init__.py
+++ backend/core/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_cors import CORS
 from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
 from flask_migrate import Migrate 
 
 load_dotenv()
@@ -13,7 +14,10 @@ CORS(app)
 SECRET_KEY = os.environ.get('SECRET_KEY')
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 db = SQLAlchemy(app)
+# app.config['SQLALCHEMY_DATABASE_URI'] likeee mysql://username:password@host:port/database_name
+
 migrate = Migrate(app, db)
 
 from core import views
--- backend/core/models.py
+++ backend/core/models.py
@@ -1,23 +1,24 @@
 from core import db
 
-class CampDetails(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(140))
-    date = db.Column(db.Date())
-    time = db.Column(db.Time())
-    category= db.Column(db.String, db.ForeignKey('category.id'))
+# class CampDetails(db.Model):
+#     CampID = db.Column(db.Integer, primary_key=True)
+#     AdminName = db.Column(db.String(90))
+#     CampName = db.Column(db.String(90))
+#     CampAddress = db.Column(db.String(90))
+#     NumberOfRefugees = db.Column(db.Integer)
+#     created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
 
-    def __repr__(self):
-         return '<Camp Details {self.title}>'
+#     def __repr__(self):
+#          return '<Camp Details {self.title}>'
 
-class RefugeeBoard(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(140))
-    date = db.Column(db.Date())
-    time = db.Column(db.Time())
-    category= db.Column(db.String, db.ForeignKey('category.id'))
+# class RefugeeBoard(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String(140))
+#     date = db.Column(db.Date())
+#     time = db.Column(db.Time())
+#     category= db.Column(db.String, db.ForeignKey('category.id'))
 
-    def __repr__(self):
-        return '<Refugee Board {self.title}>'
+#     def __repr__(self):
+#         return '<Refugee Board {self.title}>'
     
 
