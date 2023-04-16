5f1854cfe0e852ebe92981b86cd841af600a8b19
Syed Hisham Akmal <hisham0502@gmail.com>
2023-04-16 14:44:18 +0530

    Frontend navbar updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,20 +7,23 @@ function App() {
   const [count, setCount] = useState(0)
   const [info, setInfo] = useState(null);
 
-  useEffect(() => {
-    fetch('http://127.0.0.1:5000/api/get')
-      .then(response => response.json())
-      .then(response => {
-        console.log(response)
-        setInfo(response);
-      })
-      .catch(error => console.log(error))
+  // useEffect(() => {
+  //   fetch('http://127.0.0.1:5000/api/get')
+  //     .then(response => response.json())
+  //     .then(response => {
+  //       console.log(response)
+  //       setInfo(response);
+  //     })
+  //     .catch(error => console.log(error))
 
-  }, [])
+  // }, [])
 
   return (
-    <div className="App">
-      {info && <p>{JSON.stringify(info)}</p>}
+    // <div className="App">
+    //   {info && <p>{JSON.stringify(info)}</p>}
+    // </div>
+    <div>
+      <h1>Yooo sup</h1>
     </div>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,54 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import About from "./pages/About"
+import Login from "./pages/Login"
+import Contact from "./pages/Contact"
+import Admin from "./pages/Admin"
+import ErrorPage from "./pages/ErrorPage"
+import Navbar from './components/Navbar';
+import Root from './Root';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "admin",
+        element: <Admin />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+    ],
+  },
+  // {
+  //   path: "/admin",
+  //   element: <Admin />,
+  // },
+  // {
+  //   path: "/about",
+  //   element: <About />,
+  // },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider
+    router={router}
+  />,
 )
