803f5aa5cbefb1869391340f9a08f156f840cf5b
Syed Hisham Akmal <hisham0502@gmail.com>
2023-04-26 15:51:02 +0530

    Navbar updated

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -25,13 +25,18 @@ function Navbar({ user, setUser }) {
                 }
 
             }} >Logout</Link>}
-            {user && <Link className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900" type='button' onClick={async (e) => {
+            {user && <button className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5  dark:bg-green-600 dark:hover:bg-green-700 m-0" type='button' onClick={async (e) => {
+                e.preventDefault()
+            }} >Edit Profile</button>}
+            {user && <Link className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-red-600 dark:hover:bg-red-700 m-0" type='button' onClick={async (e) => {
                 e.preventDefault()
                 try {
                     const con = confirm("Do you want to delete the camp?")
-                    if (con) await deleteAcc();
-                    setUser(null)
-                    navigate('/')
+                    if (con) {
+                        await deleteAcc();
+                        setUser(null)
+                        navigate('/')
+                    }
                 } catch (err) {
                     console.log(err)
                 }
